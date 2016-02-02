2528315cb61c40b906d061231bc5a47706056047
Mike <mikeadkison@gmail.com>
2016-02-02 09:20:48 -0500

    names extracted from html

---
+++ Finder.java
@@ -0,0 +1,23 @@
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+public class Finder {
+	public static void main(String[] args) throws IOException {
+		Document doc = Jsoup.connect("http://logs.tf/1240238").get();
+		Element playerTable = doc.select("table[id=\"players\"]").get(0).select("tbody").get(0);
+		//System.out.println(doc.select("i[class^=\"classicon\"]").size());
+		for (Element row: playerTable.select("tr")) { //1 row per player
+			Elements tds = row.select("td");
+
+			Element nameCell = tds.get(1);
+			Element nameLink = nameCell.select("a[class=\"dropdown-toggle\"]").get(0);
+			System.out.println(nameLink.text());
+        }
+	}
+}
