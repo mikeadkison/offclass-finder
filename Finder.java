fa61367f3d6d766d6410c9c702dd3ace489baf4d
Mike <mikeadkison@gmail.com>
2016-02-02 11:36:58 -0500

    simple gui added

--- Finder.java
+++ Finder.java
@@ -11,33 +11,74 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Finder {
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.ScrollPane;
+import javafx.application.Application;
+import javafx.scene.text.Text;
+
+public class Finder extends Application {
 	private static final List<String> OFFCLASSES = new ArrayList<>(); //list of classes which are considered OFFCLASSES
 	private static final double MAX_OFFCLASS_PERCENT = 40.;
-	public static void main(String[] args) throws IOException {
+	private static String displayedText;
+	private static final int TIMEOUT = 10 * 1000;
+	private static final int NUM_PAGES = 15; //number of pages on logs.tf to explore for logs
+
+	public static void main(String[] args) {
 		OFFCLASSES.add("pyro");
 		OFFCLASSES.add("heavy");
 		OFFCLASSES.add("sniper");
 		OFFCLASSES.add("spy");
 		OFFCLASSES.add("engineer");
 
-		Document mainPage = Jsoup.connect("http://logs.tf").get();
-		Element logsTableBody = mainPage.select("tbody").first();
-		for (Element row: logsTableBody.select("tr")) {
-			Element logLink = row.select("a").first();
-			String logName = logLink.text();
-			String urlEnding = logLink.attr("href");
-			String url = "http://logs.tf" + urlEnding;
+		String baseURL = "http://logs.tf/?p=";
+		for (int i = 1; i <= NUM_PAGES; i++) {
+			String logsListURL = baseURL + String.valueOf(i);
 
-			if (logName.contains("TF2Center")) {
-				analyzeLog(url);
+			try {
+				Document mainPage = Jsoup.connect(logsListURL).timeout(TIMEOUT).get();
+				Element logsTableBody = mainPage.select("tbody").first();
+				for (Element row: logsTableBody.select("tr")) {
+					Element logLink = row.select("a").first();
+					String logName = logLink.text();
+					String urlEnding = logLink.attr("href");
+					String url = "http://logs.tf" + urlEnding;
+					String gameFormat = row.select("td").get(2).text();
+					if (logName.contains("TF2Center") && gameFormat.equals("6v6")) {
+						if (analyzeLog(url).length() > 0) {
+							displayedText += url + "\n\n";
+							displayedText += analyzeLog(url) + "---------------------------------------------------------\n";
+						}
+						
+					}
+					
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			
 		}
+
+		launch(args);
 	}
 
+	@Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello Brightly!");
+        
+        ScrollPane root = new ScrollPane();
+        Text textNode = new Text(displayedText);
+        root.setContent(textNode);
+        primaryStage.setScene(new Scene(root, 700, 700));
+        primaryStage.show();
+    }
 
-	private static void analyzeLog(String url) throws IOException {
+
+    /**
+     * returns empty string if no offclassers, else returns information about the offclassers
+     */
+	private static String analyzeLog(String url) throws IOException {
+		String toReturn = "";
 		Document doc = Jsoup.connect(url).get();
 		Element playerTable = doc.select("table[id=\"players\"]").get(0).select("tbody").first();
 		//System.out.println(doc.select("i[class^=\"classicon\"]").size());
@@ -46,7 +87,7 @@ public class Finder {
 
 			Element nameCell = tds.get(1);
 			Element nameLink = nameCell.select("a[class=\"dropdown-toggle\"]").first();
-			System.out.println("name: " + nameLink.text());
+			
 
 			Element classesCell = tds.get(2);
 			Elements classes = classesCell.select("i[class^=\"classicon\"");
@@ -54,13 +95,11 @@ public class Finder {
 			int totalSecs = 0;
 			int totalOffclassSecs = 0;
 			for (Element currClass: classes) { //for each class that the player played
-				System.out.println("\t" + currClass.className());
 				String timePlayedTableStr = currClass.attr("data-content");
 
 				Document tableDoc = Jsoup.parse(timePlayedTableStr);
 				Element timePlayedTable = tableDoc.select("table").first();
 				String timePlayedStr = timePlayedTable.select("tbody").first().select("tr").first().select("td").first().text();
-				System.out.println("\t\t" + timePlayedStr);
 				String[] minsAndSecs = timePlayedStr.split(":");
 				int timePlayed = Integer.parseInt(minsAndSecs[0]) * 60 + Integer.parseInt(minsAndSecs[1]); //time played by this player on this class in seconds
 				totalSecs += timePlayed;
@@ -74,13 +113,18 @@ public class Finder {
 
 			double offclassPercent = Math.floor((double)totalOffclassSecs / totalSecs * 100);
 
-			System.out.println("\t offclass percentage: " + offclassPercent + "%");
+			
 
 			if (offclassPercent > MAX_OFFCLASS_PERCENT) {
+				toReturn += "name: " + nameLink.text();
+				toReturn += "\n\t offclass percentage: " + offclassPercent + "%";
 				Element linksList = nameCell.select("ul[class=\"dropdown-menu\"]").first();
 				String tf2centerProfile = linksList.select("li").get(5).select("a").first().attr("href");
-				System.out.println("\t" + tf2centerProfile);
+				toReturn += "\n\t" + tf2centerProfile;
+				toReturn += "\n";
 			}
         }
+
+        return toReturn;
 	}
 }
