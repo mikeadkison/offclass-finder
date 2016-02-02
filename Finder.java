230221b8c093749b903a5682514aec325133208c
Mike <mikeadkison@gmail.com>
2016-02-02 10:10:57 -0500

    calculate offclass percentages

--- Finder.java
+++ Finder.java
@@ -7,8 +7,20 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Finder {
+	private static final List<String> offclasses = new ArrayList<>(); //list of classes which are considered offclasses
+
 	public static void main(String[] args) throws IOException {
+		offclasses.add("pyro");
+		offclasses.add("heavy");
+		offclasses.add("sniper");
+		offclasses.add("spy");
+		offclasses.add("engineer");
+
 		Document doc = Jsoup.connect("http://logs.tf/1240238").get();
 		Element playerTable = doc.select("table[id=\"players\"]").get(0).select("tbody").first();
 		//System.out.println(doc.select("i[class^=\"classicon\"]").size());
@@ -23,6 +35,7 @@ public class Finder {
 			Elements classes = classesCell.select("i[class^=\"classicon\"");
 
 			int totalSecs = 0;
+			int totalOffclassSecs = 0;
 			for (Element currClass: classes) { //for each class that the player played
 				System.out.println("\t" + currClass.className());
 				String timePlayedTableStr = currClass.attr("data-content");
@@ -31,10 +44,18 @@ public class Finder {
 				Element timePlayedTable = tableDoc.select("table").first();
 				String timePlayedStr = timePlayedTable.select("tbody").first().select("tr").first().select("td").first().text();
 				System.out.println("\t\t" + timePlayedStr);
+				String[] minsAndSecs = timePlayedStr.split(":");
+				int timePlayed = Integer.parseInt(minsAndSecs[0]) * 60 + Integer.parseInt(minsAndSecs[1]); //time played by this player on this class in seconds
+				totalSecs += timePlayed;
 
+				for (String offclass: offclasses) {
+					if (currClass.className().contains(offclass)) { //the player has played this offclass
+						totalOffclassSecs += timePlayed;
+					}
+				}
 			}
 
-
+			System.out.println("\t offclass percentage: " + Math.floor((double)totalOffclassSecs / totalSecs * 100) + "%");
         }
 	}
 }
